1c4c8d1f8be2288af649c5ba07c1497d8e1f8ee3
J Cole Morrison <me@jcolemorrison.com>
2023-06-04 14:21:07 -0700

    aws client vpc and vars

---
+++ aws/client/locals.tf
@@ -0,0 +1,5 @@
+locals {
+  project_tag = var.aws_default_tags.Project
+  public_cidr_blocks = [for i in range(var.vpc_public_subnet_count) : cidrsubnet(var.vpc_cidr, 4, i)]
+  private_cidr_blocks = [for i in range(var.vpc_private_subnet_count) : cidrsubnet(var.vpc_cidr, 4, i+var.vpc_public_subnet_count)]
+}
