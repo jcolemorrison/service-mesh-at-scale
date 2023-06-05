2ae8776b2c177d0c85cba92e335e88b650371af8
J Cole Morrison <me@jcolemorrison.com>
2023-06-04 17:17:16 -0700

    output values needed by other workspaces

--- aws/consul/hcp.tf
+++ aws/consul/hcp.tf
@@ -1,4 +1,4 @@
-resource "hcp_consul_cluster" "example" {
+resource "hcp_consul_cluster" "smas" {
   cluster_id = "smas-aws-consul-cluster"
   hvn_id     = hcp_hvn.hvn_service_mesh_at_scale.hvn_id
   tier       = "standard"
