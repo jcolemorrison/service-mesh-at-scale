dd058b19e63cead57693ca39eebd9555bb270f5b
J Cole Morrison <me@jcolemorrison.com>
2023-06-06 11:23:34 -0700

    remove hcp consul clusters

--- aws/consul/hcp.tf
+++ aws/consul/hcp.tf
@@ -1,15 +1,15 @@
-resource "hcp_consul_cluster" "aws" {
-  cluster_id = "aws"
-  hvn_id     = hcp_hvn.hvn_service_mesh_at_scale.hvn_id
-  tier       = "development"
-  project_id = var.hcp_project_id
-  size = "x_small"
-}
+# resource "hcp_consul_cluster" "aws" {
+#   cluster_id = "aws"
+#   hvn_id     = hcp_hvn.hvn_service_mesh_at_scale.hvn_id
+#   tier       = "development"
+#   project_id = var.hcp_project_id
+#   size = "x_small"
+# }
 
-resource "hcp_consul_cluster" "aws_peer" {
-  cluster_id = "aws-peer"
-  hvn_id     = hcp_hvn.hvn_service_mesh_at_scale.hvn_id
-  tier       = "development"
-  project_id = var.hcp_project_id
-  size = "x_small"
-}
+# resource "hcp_consul_cluster" "aws_peer" {
+#   cluster_id = "aws-peer"
+#   hvn_id     = hcp_hvn.hvn_service_mesh_at_scale.hvn_id
+#   tier       = "development"
+#   project_id = var.hcp_project_id
+#   size = "x_small"
+# }
--- aws/consul/outputs.tf
+++ aws/consul/outputs.tf
@@ -13,40 +13,40 @@ output "ram_resource_arns" {
   value = zipmap(aws_ram_principal_association.tgw[*].principal, aws_ram_resource_share.tgw[*].arn)
 }
 
-output "hcp_consul_cluster_id" {
-  description = "Cluster ID of the HCP Consul Cluster"
-  value = hcp_consul_cluster.aws.cluster_id
-}
+# output "hcp_consul_cluster_id" {
+#   description = "Cluster ID of the HCP Consul Cluster"
+#   value = hcp_consul_cluster.aws.cluster_id
+# }
 
-output "hcp_consul_root_token_secret_id" {
-  description = "root token ID of the consul cluster needed in other ECS projects for ACL bootstrapping"
-  value = hcp_consul_cluster.aws.consul_root_token_secret_id
-  sensitive = true
-}
+# output "hcp_consul_root_token_secret_id" {
+#   description = "root token ID of the consul cluster needed in other ECS projects for ACL bootstrapping"
+#   value = hcp_consul_cluster.aws.consul_root_token_secret_id
+#   sensitive = true
+# }
 
-output "hcp_consul_gossip_key" {
-  description = "gossip key used by all clients"
-  value = jsondecode(base64decode(hcp_consul_cluster.aws.consul_config_file))["encrypt"]
-  sensitive = true
-}
+# output "hcp_consul_gossip_key" {
+#   description = "gossip key used by all clients"
+#   value = jsondecode(base64decode(hcp_consul_cluster.aws.consul_config_file))["encrypt"]
+#   sensitive = true
+# }
 
-output "hcp_consul_ca_file" {
-  description = "CA file used by consul clients for mtls.  comes base64 encoded"
-  value = hcp_consul_cluster.aws.consul_ca_file
-  sensitive = true
-}
+# output "hcp_consul_ca_file" {
+#   description = "CA file used by consul clients for mtls.  comes base64 encoded"
+#   value = hcp_consul_cluster.aws.consul_ca_file
+#   sensitive = true
+# }
 
-output "hcp_consul_public_endpoint" {
-  description = "public endpoint of the HCP consul cluster"
-  value = hcp_consul_cluster.aws.consul_public_endpoint_url
-}
+# output "hcp_consul_public_endpoint" {
+#   description = "public endpoint of the HCP consul cluster"
+#   value = hcp_consul_cluster.aws.consul_public_endpoint_url
+# }
 
-output "hcp_consul_private_endpoint" {
-  description = "private endpoint of the HCP consul cluster"
-  value = hcp_consul_cluster.aws.consul_private_endpoint_url
-}
+# output "hcp_consul_private_endpoint" {
+#   description = "private endpoint of the HCP consul cluster"
+#   value = hcp_consul_cluster.aws.consul_private_endpoint_url
+# }
 
-output "hcp_datacenter" {
-  description = "datacenter of the cluster"
-  value = hcp_consul_cluster.aws.datacenter
-}
+# output "hcp_datacenter" {
+#   description = "datacenter of the cluster"
+#   value = hcp_consul_cluster.aws.datacenter
+# }
