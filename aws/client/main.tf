29270a3d2cd173983abe0e1d9ae9eb6dd4836426
J Cole Morrison <me@jcolemorrison.com>
2023-06-06 14:58:11 -0700

    remove duplicate data source

--- aws/client/main.tf
+++ aws/client/main.tf
@@ -20,12 +20,3 @@ provider "aws" {
 }
 
 provider "hcp" {}
-
-# Note: filter out wavelength zones if they're enabled in the account being deployed to.
-data "aws_availability_zones" "available" {
-  state = "available"
-  filter {
-    name   = "group-name"
-    values = [var.aws_default_region]
-  }
-}
