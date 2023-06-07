9730218653863c2b319196bacc099585ff38f3f2
J Cole Morrison <me@jcolemorrison.com>
2023-06-07 12:23:14 -0700

    export services to azure

--- aws/consul/consul-config.tf
+++ aws/consul/consul-config.tf
@@ -16,6 +16,24 @@ resource "consul_config_entry" "exported_services" {
   })
 }
 
+resource "consul_config_entry" "exported_services_azure" {
+  kind = "exported-services"
+  name = "default"
+
+  config_json = jsonencode({
+    Services = [
+      {
+        Name      = "client"
+        Consumers = [
+            {
+                Peer  = "gcp-default"
+            },
+        ]
+      }
+    ] 
+  })
+}
+
 resource "consul_config_entry" "shipping_azure" {
   kind = "service-resolver"
   name = "shipping-azure"
