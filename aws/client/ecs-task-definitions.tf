001f7b862313380fa16cff498419e562b3cc38f8
J Cole Morrison <me@jcolemorrison.com>
2023-06-06 20:56:25 -0700

    back to private endpoint again

--- aws/client/ecs-task-definitions.tf
+++ aws/client/ecs-task-definitions.tf
@@ -47,7 +47,7 @@ module "client" {
   # All settings required by the mesh-task module
   acls = true
   enable_acl_token_replication = true
-  consul_http_addr             = "${data.hcp_consul_cluster.aws.consul_public_endpoint_url}:8501"
+  consul_http_addr             = "${data.hcp_consul_cluster.aws.consul_private_endpoint_url}:8501"
 
   consul_datacenter = data.hcp_consul_cluster.aws.datacenter
   consul_primary_datacenter = data.hcp_consul_cluster.aws.datacenter # required for mesh gateways?
