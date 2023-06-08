4828063cd316a8ac7c35b4dec3149e10d54473a0
J Cole Morrison <me@jcolemorrison.com>
2023-06-07 23:10:24 -0700

    try exported services config for enterprise partitions

--- azure/config.tf
+++ azure/config.tf
@@ -1,33 +1,66 @@
+# resource "consul_config_entry" "exported_services" {
+#   name = "default"
+#   kind = "exported-services"
+
+#   config_json = jsonencode({
+#     Services = [{
+#       Name = "tea-catalog"
+#       Consumers = [{
+#         Peer = "${var.peer_datacenter}-${var.peer_partition}"
+#       },{
+#         Peer = "gcp-${var.peer_partition}"
+#       }
+#       ]
+#       },
+#       {
+#         Name = "tea-customers"
+#         Consumers = [{
+#           Peer = "${var.peer_datacenter}-${var.peer_partition}"
+#         },{
+#           Peer = "gcp-${var.peer_partition}"
+#         }
+#       ]
+#       },
+#       {
+#         Name = "shipping"
+#         Consumers = [{
+#           Peer = "${var.peer_datacenter}-${var.peer_partition}"
+#         },
+#         {
+#           Peer = "gcp-${var.peer_partition}"
+#         }
+#         ]
+#     }]
+#   })
+# }
+
 resource "consul_config_entry" "exported_services" {
-  name = "default"
   kind = "exported-services"
+  partition = "default"
+  name = "default"
 
   config_json = jsonencode({
     Services = [{
       Name = "tea-catalog"
+      Namespace = "default"
       Consumers = [{
-        Peer = "${var.peer_datacenter}-${var.peer_partition}"
-      },{
-        Peer = "gcp-${var.peer_partition}"
+        Partition = "${var.peer_partition}"
       }
       ]
       },
       {
         Name = "tea-customers"
+        Namespace = "default"
         Consumers = [{
-          Peer = "${var.peer_datacenter}-${var.peer_partition}"
-        },{
-          Peer = "gcp-${var.peer_partition}"
+          Partition = "${var.peer_partition}"
         }
       ]
       },
       {
         Name = "shipping"
+        Namespace = "default"
         Consumers = [{
-          Peer = "${var.peer_datacenter}-${var.peer_partition}"
-        },
-        {
-          Peer = "gcp-${var.peer_partition}"
+          Partition = "${var.peer_partition}"
         }
         ]
     }]
