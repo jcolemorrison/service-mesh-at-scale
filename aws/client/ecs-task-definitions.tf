33108586ccf1d884a58d0101c769130e8eceadb6
J Cole Morrison <me@jcolemorrison.com>
2023-06-08 21:47:47 -0700

    add upstream to client service

--- aws/client/ecs-task-definitions.tf
+++ aws/client/ecs-task-definitions.tf
@@ -38,7 +38,7 @@ module "client" {
         },
         {
           name = "UPSTREAM_URIS" # Fake service upstream service to call to
-          value = "http://localhost:1234,http://localhost:1235" # point all upstreams to the proxy
+          value = "http://localhost:1234,http://localhost:1235,http://localhost:1236" # point all upstreams to the proxy
         }
       ]
     }
@@ -58,6 +58,10 @@ module "client" {
         meshGateway = {
           mode = "local"
         }
+    },
+    {
+      destinationName = "catalog"
+      localBindPort = 1236
     }
   ]
 
