4c8306950e905f8bb95e7c274f4c671dcc34d756
J Cole Morrison <me@jcolemorrison.com>
2023-06-09 21:32:09 -0700

    comment out exported and services for demo

--- azure/config.tf
+++ azure/config.tf
@@ -1,73 +1,73 @@
-resource "consul_config_entry" "exported_services" {
-  name = "default"
-  kind = "exported-services"
+# resource "consul_config_entry" "exported_services" {
+#   name = "default"
+#   kind = "exported-services"
 
-  config_json = jsonencode({
-    Services = [
-    {
-      Name = "tea-catalog"
-      Consumers = [{
-        Peer = "${var.peer_datacenter}-${var.peer_partition}" # aws-default
-      },{
-          Peer = "gcp-${var.peer_partition}" # gcp-default
-      }]
-    },
-    {
-      Name = "tea-customers"
-      Consumers = [{
-        Peer = "${var.peer_datacenter}-${var.peer_partition}"
-      },{
-        Peer = "gcp-${var.peer_partition}"
-      }]
-    },
-    {
-      Name = "shipping"
-      Consumers = [{
-        Peer = "${var.peer_datacenter}-${var.peer_partition}"
-      },
-      {
-        Peer = "gcp-${var.peer_partition}"
-      }]
-    }
-    ]
-  })
-}
+#   config_json = jsonencode({
+#     Services = [
+#     {
+#       Name = "tea-catalog"
+#       Consumers = [{
+#         Peer = "${var.peer_datacenter}-${var.peer_partition}" # aws-default
+#       },{
+#           Peer = "gcp-${var.peer_partition}" # gcp-default
+#       }]
+#     },
+#     {
+#       Name = "tea-customers"
+#       Consumers = [{
+#         Peer = "${var.peer_datacenter}-${var.peer_partition}"
+#       },{
+#         Peer = "gcp-${var.peer_partition}"
+#       }]
+#     },
+#     {
+#       Name = "shipping"
+#       Consumers = [{
+#         Peer = "${var.peer_datacenter}-${var.peer_partition}"
+#       },
+#       {
+#         Peer = "gcp-${var.peer_partition}"
+#       }]
+#     }
+#     ]
+#   })
+# }
 
-resource "consul_config_entry" "shipping_intention" {
-  name = "shipping"
-  kind = "service-intentions"
+# resource "consul_config_entry" "shipping_intention" {
+#   name = "shipping"
+#   kind = "service-intentions"
 
-  config_json = jsonencode({
-    Sources = [{
-      Name = "orders"
-      Peer = "aws-default"
-      Action = "allow"
-    }]
-  })
-}
+#   config_json = jsonencode({
+#     Sources = [{
+#       Name = "orders"
+#       Peer = "aws-default"
+#       Action = "allow"
+#     }]
+#   })
+# }
 
-resource "consul_config_entry" "tea_catalog_intention" {
-  name = "tea-catalog"
-  kind = "service-intentions"
+# resource "consul_config_entry" "tea_catalog_intention" {
+#   name = "tea-catalog"
+#   kind = "service-intentions"
 
-  config_json = jsonencode({
-    Sources = [{
-      Name = "catalog"
-      Peer = "aws-default"
-      Action = "allow"
-    }]
-  })
-}
+#   config_json = jsonencode({
+#     Sources = [{
+#       Name = "catalog"
+#       Peer = "aws-default"
+#       Action = "allow"
+#     }]
+#   })
+# }
 
-resource "consul_config_entry" "tea_customers_intention" {
-  name = "tea-customers"
-  kind = "service-intentions"
+# resource "consul_config_entry" "tea_customers_intention" {
+#   name = "tea-customers"
+#   kind = "service-intentions"
 
-  config_json = jsonencode({
-    Sources = [{
-      Name = "customers"
-      Peer = "aws-default"
-      Action = "allow"
-    }]
-  })
-}
+#   config_json = jsonencode({
+#     Sources = [{
+#       Name = "customers"
+#       Peer = "aws-default"
+#       Action = "allow"
+#     }]
+#   })
+# }
--- gcp/consul/exported-services.tf
+++ gcp/consul/exported-services.tf
@@ -1,25 +1,25 @@
-resource "consul_config_entry" "exported_services" {
-  name = "default"
-  kind = "exported-services"
+# resource "consul_config_entry" "exported_services" {
+#   name = "default"
+#   kind = "exported-services"
 
-  config_json = jsonencode({
-    Services = [{
-      Name = "coffee-catalog"
-      Consumers = [{
-        Peer = "${var.peer_datacenter}-${var.peer_partition}" # aws-default
-      }]
-      },
-      {
-        Name = "coffee-customers"
-        Consumers = [{
-          Peer = "${var.peer_datacenter}-${var.peer_partition}"
-        }]
-      },
-      {
-        Name = "loyalty"
-        Consumers = [{
-          Peer = "${var.peer_datacenter}-${var.peer_partition}"
-        }]
-    }]
-  })
-}
+#   config_json = jsonencode({
+#     Services = [{
+#       Name = "coffee-catalog"
+#       Consumers = [{
+#         Peer = "${var.peer_datacenter}-${var.peer_partition}" # aws-default
+#       }]
+#       },
+#       {
+#         Name = "coffee-customers"
+#         Consumers = [{
+#           Peer = "${var.peer_datacenter}-${var.peer_partition}"
+#         }]
+#       },
+#       {
+#         Name = "loyalty"
+#         Consumers = [{
+#           Peer = "${var.peer_datacenter}-${var.peer_partition}"
+#         }]
+#     }]
+#   })
+# }
