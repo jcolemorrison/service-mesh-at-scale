bcf5febc60fab27d18f28335721265437e0794bb
J Cole Morrison <me@jcolemorrison.com>
2023-06-09 23:06:13 -0700

    demo export services and intentions from gcp

--- gcp/consul/intentions.tf
+++ gcp/consul/intentions.tf
@@ -1,50 +1,50 @@
-# resource "consul_config_entry" "service_intentions_loyalty" {
-#   name = "loyalty"
-#   kind = "service-intentions"
+resource "consul_config_entry" "service_intentions_loyalty" {
+  name = "loyalty"
+  kind = "service-intentions"
 
-#   config_json = jsonencode({
-#     Sources = [
-#       {
-#         Action     = "allow"
-#         Name       = "client"
-#         Peer       = "aws-default"
-#         Precedence = 9
-#         Type       = "consul"
-#       }
-#     ]
-#   })
-# }
+  config_json = jsonencode({
+    Sources = [
+      {
+        Action     = "allow"
+        Name       = "client"
+        Peer       = "aws-default"
+        Precedence = 9
+        Type       = "consul"
+      }
+    ]
+  })
+}
 
-# resource "consul_config_entry" "service_intentions_coffee_catalog" {
-#   name = "coffee-catalog"
-#   kind = "service-intentions"
+resource "consul_config_entry" "service_intentions_coffee_catalog" {
+  name = "coffee-catalog"
+  kind = "service-intentions"
 
-#   config_json = jsonencode({
-#     Sources = [
-#       {
-#         Action     = "allow"
-#         Name       = "catalog"
-#         Peer       = "aws-default"
-#         Precedence = 9
-#         Type       = "consul"
-#       }
-#     ]
-#   })
-# }
+  config_json = jsonencode({
+    Sources = [
+      {
+        Action     = "allow"
+        Name       = "catalog"
+        Peer       = "aws-default"
+        Precedence = 9
+        Type       = "consul"
+      }
+    ]
+  })
+}
 
-# resource "consul_config_entry" "service_intentions_coffee_customers" {
-#   name = "coffee-customers"
-#   kind = "service-intentions"
+resource "consul_config_entry" "service_intentions_coffee_customers" {
+  name = "coffee-customers"
+  kind = "service-intentions"
 
-#   config_json = jsonencode({
-#     Sources = [
-#       {
-#         Action     = "allow"
-#         Name       = "customers"
-#         Peer       = "aws-default"
-#         Precedence = 9
-#         Type       = "consul"
-#       }
-#     ]
-#   })
-# }
+  config_json = jsonencode({
+    Sources = [
+      {
+        Action     = "allow"
+        Name       = "customers"
+        Peer       = "aws-default"
+        Precedence = 9
+        Type       = "consul"
+      }
+    ]
+  })
+}
